f7c618f15e6819d956272a17d8483004ef63c9c9
hideko-dev <hideko332200@gmail.com>
2024-11-10 11:06:57 +0900

    lat

--- src/main/kotlin/rest/hideko/kust/inventory/KInventory.kt
+++ src/main/kotlin/rest/hideko/kust/inventory/KInventory.kt
@@ -12,36 +12,47 @@ import rest.hideko.kust.Kust
 class KInventory(private val title: String, row: Int): Listener {
     private val inventory = Bukkit.createInventory(null, row * 9, title)
     private val ignoredSlots = mutableSetOf<Int>()
-    private val slotActions = mutableMapOf<Int, (InventoryClickEvent) -> Unit>()
+    private val slotActions = mutableMapOf<Int, (KInventoryClick) -> Unit>()
 
     init {
         Kust.plugin.server.pluginManager.registerEvents(this, Kust.plugin)
     }
+
     fun inventory(setup: (Turn) -> Unit): KInventory {
         val turn = Turn(inventory)
         setup(turn)
         return this
     }
+
     inner class Turn(private val inv: Inventory) {
-        fun set(slot: Int, item: ItemStack, onClick: (InventoryClickEvent) -> Unit) {
+        fun set(slot: Int, item: ItemStack, onClick: (KInventoryClick) -> Unit) {
             inv.setItem(slot, item)
             slotActions[slot] = onClick
         }
     }
+
     fun ignore(vararg slots: Int): KInventory {
         ignoredSlots.addAll(slots.toSet())
         return this
     }
+
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
         if (event.view.title == title) {
             if (ignoredSlots.contains(event.slot)) return
             event.isCancelled = true
-            slotActions[event.slot]?.invoke(event)
+            val click = KInventoryClick(event)
+            slotActions[event.slot]?.invoke(click)
         }
     }
+
     fun open(player: Player): KInventory {
         player.openInventory(inventory)
         return this
+    }
+
+    class KInventoryClick(private val event: InventoryClickEvent) {
+        val player: Player
+            get() = event.whoClicked as Player
     }
 }
